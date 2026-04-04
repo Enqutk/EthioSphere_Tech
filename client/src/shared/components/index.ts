b706a7a0324df3400e95dd8662d8b82b57b11263
Enqutk <enkukokob@gmail.com>
2026-04-04 14:42:15 +0300

    Add TerminalBackdrop and AppFrame exports: Include new components in the shared index file to improve modularity and facilitate their usage across the application.

--- client/src/shared/components/index.ts
+++ client/src/shared/components/index.ts
@@ -1,3 +1,5 @@
 export { Nav } from './Nav';
 export { AuthProvider, useAuth, getStoredToken } from './AuthProvider';
 export { FollowCreatorActions } from './FollowCreatorActions';
+export { TerminalBackdrop } from './TerminalBackdrop';
+export { AppFrame } from './AppFrame';
