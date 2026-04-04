99f9be377c791fc8d53e877d68b86cd4526d39d8
Enqutk <enkukokob@gmail.com>
2026-04-04 12:51:38 +0300

    Refactor main entry point: Replace AuthProvider with AppProviders for improved architecture and organization, aligning with the new application structure.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import { AuthProvider } from './components/AuthProvider';
+import { AppProviders } from '@/app/providers';
+import App from '@/app/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider>
+      <AppProviders>
         <App />
-      </AuthProvider>
+      </AppProviders>
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
